2405f12d74507f98dc2be522e14f36800a1c389f
Vinicius Gomes de Oliveira <Vinicius52280326@edu.df.senac.br>
2025-03-31 09:42:45 -0300

    hotfix: securityfilterchain

---
+++ src/main/java/com/projeto/ads/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.projeto.ads.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projeto.ads.model.Usuario;
+
+public interface UserRepository {
+	
+}//fim interface
