7bc1a7eb68212d963998bf3ea4fe0a68cb6eb7c3
Vinicius Gomes de Oliveira <Vinicius52280326@edu.df.senac.br>
2025-03-31 09:30:10 -0300

    update: packages of project

---
+++ src/main/java/com/projeto/ads/model/Role.java
@@ -0,0 +1,52 @@
+package com.projeto.ads.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name="roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue (strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(unique=true)
+	private String nome;
+	
+	@OneToMany(mappedBy = "role")
+	private List<Usuario> usuarios;
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
